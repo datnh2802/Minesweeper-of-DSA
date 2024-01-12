b3fe6466b38ff6b05f9442c8b64135c0b61e1a2d
Nguyễn Hoàng Đạt <131468354+datnh2802@users.noreply.github.com>
2024-01-12 21:44:07 +0700

    Update ITrans.java

--- src/gui/ITrans.java
+++ src/gui/ITrans.java
@@ -1,16 +1,11 @@
 package gui;
  
 import logic.Square;
-
-import java.util.List;
-
+ 
 public interface ITrans {
   Square[][] getListSquare();
+ 
   void play(int x, int y);
   void target(int x, int y);
   void restart();
-  void undo();
-
-  void saveToStatusStack();
-
 }
